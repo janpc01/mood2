d3614ebbcfd1a73dfe35088ea2524a4e9c042667
janpc01 <janpc01@gmail.com>
2025-03-14 13:53:16 -0600

    Removed email verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :board_memberships
   has_many :boards, through: :board_memberships
